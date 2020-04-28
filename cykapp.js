d018bd36936640d159119434dced653f4303ff14
deficts <diegoar7@live.com>
2020-04-27 20:25:44 -0500

    V1

--- cykapp.js
+++ cykapp.js
@@ -1,5 +1,6 @@
-var roots = [];
-var productions = [];
+var roots = [] //= ["S", "A", "B", "C"];
+var productions = [] //= ["AB|BC", "BA|a", "CC|b", "AB|a"];
+var chart;
 
 function grammarToHashMap() {
   var hashMap = {};
@@ -16,7 +17,7 @@ function grammarToHashMap() {
   return hashMap;
 }
 
-function createTable(word, n) {
+function createTable(n) {
   const t = [];
   for (let i = 0; i < n; i++) {
     var st = []
@@ -28,14 +29,26 @@ function createTable(word, n) {
   return t;
 }
 
-function combine(x,y){
+function createIndexes(n) {
+  const t = [];
+  for (let i = 0; i < n; i++) {
+    var st = []
+    for (let j = 0; j < n - i; j++) {
+      st.push(new Map());
+    }
+    t.push(st);
+  }
+  return t;
+}
+
+function combine(x, y) {
   com = new Set();
-  if(x.size == 0 || y.size == 0){
+  if (x.size == 0 || y.size == 0) {
     return com;
   }
-  for(i of x){
-    for(j of y){
-      com.add(i+j);
+  for (i of x) {
+    for (j of y) {
+      com.add(i + j);
     }
   }
   return com;
@@ -43,32 +56,139 @@ function combine(x,y){
 
 function cykParser(word) {
   const hashMap = grammarToHashMap();
+  console.log(hashMap);
   var len = word.length;
-  var t = createTable(word, len);
+  var t = createTable(len);
+  var indexes = createIndexes(len);
   //Primera iteración checar terminales
   for (let i = 0; i < len; i++) {
     if (hashMap[word[i]]) {
-      for (let j=0; j<hashMap[word[i]].length;j++) {
+      for (let j = 0; j < hashMap[word[i]].length; j++) {
         t[0][i].add(hashMap[word[i]][j]);
+        indexes[0][i][hashMap[word[i]][j]] = word[i];
+        //if (indexes[0][i][word[i]][j]) {
+        //  indexes[0][i][word[i]][j].push(i,j,hashMap[word[i]][j]);
+        //} else {
+        //  indexes[0][i][word[i]][j] = [i,j,hashMap[word[i]][j]];
+        //}
       }
     }
+
   }
   //cyk
-  for (let i=1;i<len;i++){
-    for(let j=0;j<len-i;j++){
-      for(let k=0; k<i;k++){
-        var com = combine(t[k][j],t[i-k-1][j+k+1])
-        for(c of com){
-          if(hashMap[c]){
-            for (let d=0; d<hashMap[c].length;d++) {
+  for (let i = 1; i < len; i++) {
+    for (let j = 0; j < len - i; j++) {
+      for (let k = 0; k < i; k++) {
+        var index1 = t[k][j];
+        var index2 = t[i - k - 1][j + k + 1];
+        var com = combine(index1, index2);
+        for (c of com) {
+          if (hashMap[c]) {
+            for (let d = 0; d < hashMap[c].length; d++) {
               t[i][j].add(hashMap[c][d]);
+              if (indexes[i][j][hashMap[c][d]]) {
+                indexes[i][j][hashMap[c][d]].push(...[k, j, i - k - 1, j + k + 1, c]);
+              } else {
+                indexes[i][j][hashMap[c][d]] = [k, j, i - k - 1, j + k + 1, c];
+              }
             }
           }
         }
       }
     }
   }
-  return t;
+  console.log(t);
+  if (t[len - 1][0].has("S")) {
+    $("#result").text("Es parte de la gramática");
+    $("#result").css('color','green');
+    if(chart){
+      chart.destroy();
+      var newChart;
+      $("#cyktree").remove();
+      $("#resultContainer").append('<div id="cyktree" class="chart mx-auto"></div>');
+      newChart = new Treant(createGraphic(createTree(t, indexes)),null,$)
+    }else{
+      chart = new Treant(createGraphic(createTree(t, indexes)),null,$)
+    }
+  } else {
+    if(chart){
+      chart.destroy();
+    }
+    $("#result").text("No es parte de la gramática");
+    $("#result").css('color','red');
+  }
+}
+
+function getNextNode(tree, table, indexes, x, y, gen, i, parent) {
+  //si es terminal
+  if (x == 0) {
+    tree[gen + "" + i] = {
+      text: gen+" -> " + indexes[x][y][gen],
+      parent: parent
+    }
+    return;
+  }
+  var index = indexes[x][y][gen];
+  tree[gen + "" + i] = {
+    text: gen,
+    parent: parent
+  }
+  getNextNode(tree, table, indexes, index[0], index[1], index[4][0], i + 1, gen + "" + i);
+  getNextNode(tree, table, indexes, index[2], index[3], index[4][1], i + 2, gen + "" + i);
+}
+
+function createTree(table, indexes) {
+  length = table.length;
+  gen = "S";
+  tree = {};
+  var i = length - 1;
+  var j = 0;
+  var index = indexes[i][j][gen];
+  tree[gen] = {
+    text: "S"
+  };
+  getNextNode(tree, table, indexes, index[0], index[1], index[4][0], 1, gen);
+  getNextNode(tree, table, indexes, index[2], index[3], index[4][1], 2, gen);
+  return tree;
+}
+
+function createGraphic(tree) {
+  console.log(tree);
+  var chart_config =[];
+  var config = {
+    container: "#cyktree",
+    connectors: {
+      type: 'step'
+    },
+    node: {
+      HTMLclass: 'node'
+    }
+  }
+  chart_config.push(config);
+  parents = {};
+  c=0;
+  for (i in tree){
+    if(c==0){
+      var node = {
+        text:{
+          title:tree[i].text
+        }
+      }
+      parents[i]=node;
+      chart_config.push(node);
+    }else{
+      var node = {
+        text:{
+          title:tree[i].text
+        },
+        parent:parents[tree[i].parent]
+      }
+      parents[i]=node;
+      chart_config.push(node);
+    }
+    c++;
+  }
+  return chart_config;
 }
 
 function saveProductions() {
